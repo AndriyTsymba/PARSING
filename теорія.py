06305c82504dc515ba8dae4156b793b5772ce323
student <a.tsymbaliuk2007@gmail.com>
2023-05-25 19:43:51 +0300

    1

--- теорія.py
+++ теорія.py
@@ -8,4 +8,31 @@ print("")
 """
 import requests
 responce = requests.get("https://coinmarketcap.com/")
-print(responce.text)
+"""print(responce.text)
+responce_parse = responce.text.split("<span>")
+for parse_elem in responce_parse:
+    if parse_elem.starttswist("$"):
+        print(parse_elem)"""
+"""import urllib.request
+opener = urllib.request.build_opener()
+response = opener.open("https://httpbin.org/get")
+print(response.read())"""
+
+"""import urllib.request
+opener = urllib.request.build_opener()
+response = opener.open("https://httpbin.org/get")
+print(response.read())"""
+
+import  requests
+response = requests.get('https://coinmarketcap.com/')
+"""print(response.text)
+response_parse =  response.text.split('<span>')
+for elem in response_parse:
+    if elem.startswith("$"):
+        print(elem)"""
+from bs4 import BeautifulSoup
+if response.status_code == 200:
+    soup = BeautifulSoup(response.text, features= "html.parser")
+    soup_list = soup.find_all('a', {'href': "/currencies/bitcoin/markets/"})
+    res = soup_list[0].find('span')
+    print(res.text)
