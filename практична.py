e44a0391bf393245321f66f2cbff529a61f9f2b5
student <a.tsymbaliuk2007@gmail.com>
2023-05-25 20:01:58 +0300

    2

---
+++ практична.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get("https://uk.wikipedia.org/")
+
+if response.status_code == 200:
+    soup = BeautifulSoup(response.content , "html.parser")
+    img = soup.find_all("img")
+    for i in img:
+        print("https://" + i["src"])
+
