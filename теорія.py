f672dc028b46aa7ff8e3d091dbe75e77a83c5e22
student <a.tsymbaliuk2007@gmail.com>
2023-05-25 18:59:37 +0300

    знайомство requests

--- теорія.py
+++ теорія.py
@@ -1,6 +1,11 @@
+"""
 # знайомство з стардатними бібліотеками
 import urllib.request
 opener = urllib.request.build_opener()
 responce = opener.open("https://httpbin.org/get")
 print(responce.read())
 print("")
+"""
+import requests
+responce = requests.get("https://coinmarketcap.com/")
+print(responce.text)
